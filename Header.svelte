50d103d1730f293140bb91f217d6e8cfac2a290d
ClassicMC <classicmclive@gmail.com>
2022-11-21 11:01:54 -0500

    v0.0.1
    Change-log
    - Basic bio and links
    - Positions for Guyot with requirements

---
+++ Header.svelte
@@ -0,0 +1,43 @@
+<script>
+</script>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://classicmc-studios.github.io/bc.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+	.header{
+		margin-left: 50px;
+		margin-right: 50px;
+	}
+	.item{
+		width:50%;
+	}
+	.item-r{
+		text-align: right;
+		width:50%;
+	}
+	.basics{text-align:Left;}
+	a{text-decotation:none; color:black;}
+</style>
+<main>
+	<div class="header">
+		<div class="w3-row">
+			<div class="w3-col item w3-padding-32">
+				<h3>
+					Easton
+				</h3>
+			</div>
+			<div class="w3-col item-r w3-padding-32">
+				<h3>
+					<a href="https://guyotjs.github.io">Guyot</a>
+					<a href="https://github.com/ClassicMC-Studios"><i class="fa fa-github"></i></a>
+					<a href="https://classicmc-studios.github.io"><i class="fa fa-link"></i></a>
+				</h3>
+			</div>
+		</div>
+		<div class="basics">
+			<h3>
+				Hallo, I am a frontend software developer who loves<br/> working on random ideas.
+			</h3>
+		</div>
+	</div>
+</main>
